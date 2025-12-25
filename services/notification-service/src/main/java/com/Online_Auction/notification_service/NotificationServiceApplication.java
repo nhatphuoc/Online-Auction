9b12b4cbdafba9f10744631eec4ae6e21faad96a
Tri.Ngo <tri.ngo@knorex.com>
2025-12-25 23:10:25 +0700

    [Notification-Service] Env Config

--- services/notification-service/src/main/java/com/Online_Auction/notification_service/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/com/Online_Auction/notification_service/NotificationServiceApplication.java
@@ -1,5 +1,10 @@
 package com.Online_Auction.notification_service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +13,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableRabbit
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Path envPath = Paths.get("../../shared/.env");
+
+		if (Files.exists(envPath)) {
+			Files.lines(envPath)
+					.map(String::trim)
+					.filter(line -> !line.isEmpty()) // bỏ dòng trống
+					.filter(line -> !line.startsWith("#")) // bỏ comment
+					.filter(line -> line.matches("^[A-Za-z_][A-Za-z0-9_]*=.*")) // KEY=VALUE
+					.forEach(line -> {
+						String[] parts = line.split("=", 2);
+						System.setProperty(parts[0], parts[1]);
+					});
+		}
+
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
